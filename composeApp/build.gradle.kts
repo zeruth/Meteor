545867bd54d65f0df79ba7f00d4df5db040fb50b
Tyler Bochard <zeruth09@gmail.com>
2024-05-09 18:32:19 -0400

    project: remove non desktopMain modules
    They aren't used arm and won't be for quite a while

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -1,50 +1,14 @@
-import com.android.build.gradle.internal.lint.AndroidLintAnalysisTask
-import com.android.build.gradle.internal.lint.LintModelWriterTask
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsCompose)
 }
 
 kotlin {
-    androidTarget {
-        compilations.all {
-            kotlinOptions {
-                jvmTarget = "11"
-            }
-        }
-    }
-    
     jvm("desktop")
-    
-    listOf(
-        iosX64(),
-        iosArm64(),
-        iosSimulatorArm64()
-    ).forEach { iosTarget ->
-        iosTarget.binaries.framework {
-            baseName = "ComposeApp"
-            isStatic = true
-        }
-    }
-    
     sourceSets {
         val desktopMain by getting
-        
-        androidMain.dependencies {
-            implementation(libs.compose.ui.tooling.preview)
-            implementation(libs.androidx.activity.compose)
-        }
-        commonMain.dependencies {
-            implementation(compose.runtime)
-            implementation(compose.foundation)
-            implementation(compose.material)
-            implementation(compose.ui)
-            implementation(compose.components.resources)
-            implementation(compose.components.uiToolingPreview)
-        }
         desktopMain.dependencies {
             implementation(projects.eventbus)
             implementation(projects.api)
@@ -72,40 +36,6 @@ kotlin {
     }
 }
 
-android {
-    namespace = "meteor.client"
-    compileSdk = libs.versions.android.compileSdk.get().toInt()
-
-    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
-    sourceSets["main"].res.srcDirs("src/androidMain/res")
-    sourceSets["main"].resources.srcDirs("src/commonMain/resources")
-
-    defaultConfig {
-        applicationId = "meteor.client"
-        minSdk = libs.versions.android.minSdk.get().toInt()
-        targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0"
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    dependencies {
-        debugImplementation(libs.compose.ui.tooling)
-    }
-}
-
 compose.desktop {
     application {
         mainClass = "meteor.Main"
@@ -122,15 +52,4 @@ compose.desktop {
             }
         }
     }
-}
-
-// Fixes for latest Android Gradle Plugin for Kotlin Multiplatform
-tasks.withType<LintModelWriterTask> {
-    if (this.name == "generateDebugUnitTestLintModel")
-        dependsOn(tasks.named("generateResourceAccessorsForAndroidUnitTest"))
-}
-
-tasks.withType<AndroidLintAnalysisTask> {
-    if (this.name == "lintAnalyzeDebug" || this.name == "lintAnalyzeDebugUnitTest")
-        dependsOn(tasks.named("generateResourceAccessorsForAndroidUnitTest"))
 }
